78b6b8498b59ac770ef68b07eaff3053dc78336b
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-21 21:33:09 +0300

    8-promises-catch.js

--- solutions/8-promises-catch.js
+++ solutions/8-promises-catch.js
@@ -1,5 +1,12 @@
 import fsp from 'fs/promises';
-
-// BEGIN
-
-// END
+export const touch = async (filepath) => {
+  try {
+    await fsp.access(filepath); // Проверяем существование файла
+  } catch (err) {
+    if (err.code === 'ENOENT') { // Файл не существует
+      await fsp.writeFile(filepath, ''); // Создаем пустой файл
+    } else {
+      throw err; // Другая ошибка - идём дальше
+    }
+  }
+};
