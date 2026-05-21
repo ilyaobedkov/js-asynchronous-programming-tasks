f4f212f1fe0f4cb5cfef97b29c3af0f5564a2c3a
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-21 21:32:37 +0300

    7-promises.js

--- solutions/7-promises.js
+++ solutions/7-promises.js
@@ -1,5 +1,11 @@
 import fsp from 'fs/promises';
-
-// BEGIN
-
-// END
+export const reverse = async (filepath) => {
+  const data = await fsp.readFile(filepath, 'utf8');
+  const lines = data.split('\n'); // Разбиваем на строки
+  const lastLineEmpty = lines[lines.length - 1] === ''; // Проверяем, была ли последняя строка пустой
+  if (lastLineEmpty) lines.pop(); // Удаляем последнюю пустую строку, если была
+  lines.reverse(); // Меняем порядок строк
+  let reversedData = lines.join('\n');
+  if (lastLineEmpty) reversedData += '\n'; // Добавляем перенос в конце, если он был
+  await fsp.writeFile(filepath, reversedData, 'utf8');
+};
