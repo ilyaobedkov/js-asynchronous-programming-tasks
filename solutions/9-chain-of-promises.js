2d7e0b70c7bb45c30b9b89cfbed8fdf169b9ede8
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-21 21:33:30 +0300

    9-chain-of-promises.js

--- solutions/9-chain-of-promises.js
+++ solutions/9-chain-of-promises.js
@@ -1,5 +1,9 @@
 import fsp from 'fs/promises';
-
-// BEGIN
-
-// END
+export const getTypes = (paths) => {
+  const promises = paths.map((path) =>
+    fsp.stat(path)
+      .then((stats) => stats.isDirectory() ? 'directory' : 'file') // Определяем тип
+      .catch(() => null) // При ошибке возвращаем null
+  );
+  return Promise.all(promises); // Ждем выполнения всех промисов
+};
