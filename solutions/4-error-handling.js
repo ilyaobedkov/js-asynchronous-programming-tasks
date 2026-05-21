dba961ec80130ae21e2f729d3d2b44b62e923c2d
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-21 21:31:21 +0300

    4-error-handling.js

--- solutions/4-error-handling.js
+++ solutions/4-error-handling.js
@@ -1,5 +1,22 @@
 import fs from 'fs';
-
-// BEGIN
-
-// END
+export const move = (source, destination, callback) => {
+  fs.readFile(source, 'utf8', (err, data) => { // Читаем исходный файл
+    if (err) {
+      callback(err); // Передаем ошибку чтения
+      return;
+    }
+    fs.writeFile(destination, data, 'utf8', (err) => { // Записываем данные в новый файл
+      if (err) {
+        callback(err); // Передаем ошибку записи
+        return;
+      }
+      fs.unlink(source, (err) => { // Удаляем исходный файл
+        if (err) {
+          callback(err); // Передаем ошибку удаления
+          return;
+        }
+        callback(null); // Успешное завершение (ошибки нет)
+      });
+    });
+  });
+};
