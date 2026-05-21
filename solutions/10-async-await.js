c7de5a8acdbe25c27b9a466fb435ce3fec445db3
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-21 21:34:21 +0300

    10-async-await.js

--- solutions/10-async-await.js
+++ solutions/10-async-await.js
@@ -1,5 +1,7 @@
 import fsp from 'fs/promises';
-
-// BEGIN
-
-// END
+export const exchange = async (filepath1, filepath2) => {
+  const data1 = await fsp.readFile(filepath1, 'utf8'); // Читаем первый файл
+  const data2 = await fsp.readFile(filepath2, 'utf8'); // Читаем второй файл
+  await fsp.writeFile(filepath1, data2, 'utf8'); // Записываем данные второго в первый
+  await fsp.writeFile(filepath2, data1, 'utf8'); // Записываем данные первого во второй
+};
