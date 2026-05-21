cf065c60bc46654f2cc3fb35f44e8a0df1c5fd68
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-21 21:31:50 +0300

    5-parallel-execution.js

--- solutions/5-parallel-execution.js
+++ solutions/5-parallel-execution.js
@@ -2,7 +2,21 @@ import path from 'path';
 import fs from 'fs';
 import _ from 'lodash';
 import async from 'async';
-
-// BEGIN
-
-// END
+export const getDirectorySize = (dirPath, callback) => {
+  fs.readdir(dirPath, (err, files) => { // Читаем содержимое директории
+    if (err) {
+      callback(err); // Передаем ошибку чтения
+      return;
+    }
+    const filePaths = files.map(file => path.join(dirPath, file)); // Формируем полные пути к файлам
+    async.map(filePaths, fs.stat, (err, stats) => { // Параллельно получаем информацию о каждом файле
+      if (err) {
+        callback(err); // Передаем ошибку
+        return;
+      }
+      const sizes = stats.filter(stat => stat.isFile()).map(stat => stat.size); // Берем размеры только файлов (исключаем поддиректории)
+      const totalSize = _.sumBy(sizes); // Суммируем размеры
+      callback(null, totalSize); // Передаем результат
+    });
+  });
+};
